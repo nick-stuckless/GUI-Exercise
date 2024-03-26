1a46d417a6b66ecd3a7bce5e7a2934860b245ed7
nick-stuckless <156607156+nick-stuckless@users.noreply.github.com>
2024-03-26 10:24:17 -0400

    second commit

--- poke_info_viewer.py
+++ poke_info_viewer.py
@@ -6,14 +6,21 @@ Description:
 Usage:
   python poke_info_viewer.py
 """
-from tkinter import *
-from tkinter import ttk
+from tkinter import Tk, ttk
 
 # Create the main window
 root = Tk()
 root.title("Pokemon Information")
 
 # TODO: Create the frames
+input = ttk.Frame(root)
+input.grid(row=0, column=0)
+
+info = ttk.LabelFrame(root, text="Info")
+info.grid(row=1, column=0)
+
+stats = ttk.LabelFrame(root, text="Stats")
+stats.grid(row=1, column=1)
 
 # TODO: Populate the user input frame with widgets
 
